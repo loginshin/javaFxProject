c2da664ea0bce03b4b805d408fb6e55ee0438e8d
JJangWoo0418 <shim010418@gmail.com>
2023-11-24 03:38:44 +0900

    feat)MainMenuFXML

--- src/main/java/com/example/javafxproject/controller/BookListController.java
+++ src/main/java/com/example/javafxproject/controller/BookListController.java
@@ -1,13 +1,17 @@
 package com.example.javafxproject.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class BookListController implements Initializable {
+
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+
 }
